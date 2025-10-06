40be84b04b15a06da3e6b1279ae42db7090a51c1
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2025-10-06 20:24:57 +0530

    rename the app names in app.json file then , style the gird tile in meals app

--- Meals App/App.js
+++ Meals App/App.js
@@ -1,19 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet  } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
   return (
+    <>
+    <StatusBar style='light' />
     <CategoriesScreen />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#a30808ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color:'#ccccccff'
-  },
+
 });
--- Meals App/components/CategoryGridTile.js
+++ Meals App/components/CategoryGridTile.js
@@ -1,11 +1,17 @@
-import { Pressable, View, Text, StyleSheet } from 'react-native'
+import { Pressable, View, Text, StyleSheet, Platform } from 'react-native'
 
 const CategoryGridTile = ({ title, color }) => {
   return (
     <View style={styles.griditem}>
-      <Pressable style={styles.button}>
-        <View style={styles.innerContainer}>
-          <Text>{title}</Text>
+      <Pressable
+        android_ripple={{ color: 'ccc' }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -21,16 +27,26 @@ const styles = StyleSheet.create({
     height: 150,
     borderRadius: 8,
     elevation: 4,
-    shadowColor :'black',
-    shadowOpacity : 0.25,
-    shadowOffset : {width : 0 , height : 2},
-    shadowRadius : 8
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
   },
   button: { flex: 1 },
+  buttonPressed: {
+    opacity: 0.5
+  },
   innerContainer: {
     flex: 1,
     padding: 16,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderRadius: 8
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18
   }
 })
