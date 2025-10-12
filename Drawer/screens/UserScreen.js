d3a36c69f5d1d6ea066965cfc1874b80635fbac7
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2025-10-12 20:53:56 +0530

    add custermizations to drawer

--- Drawer/screens/UserScreen.js
+++ Drawer/screens/UserScreen.js
@@ -1,25 +1,29 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native'
 
-function UserScreen() {
+function UserScreen ({ navigation }) {
+  function openDrawerHandler () {
+    navigation.toggleDrawer()
+  }
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
+      <Button title='Open Drawer' onPress={openDrawerHandler} />
     </View>
-  );
+  )
 }
 
-export default UserScreen;
+export default UserScreen
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   highlight: {
     fontWeight: 'bold',
-    color: '#eb1064',
-  },
-});
+    color: '#eb1064'
+  }
+})
