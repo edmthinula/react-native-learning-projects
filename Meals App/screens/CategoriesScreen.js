d39a9a63d97e03bce379319c1056d13749ef2965
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2025-10-27 00:42:43 +0530

    meals app , sdk version update to 54 from 53

--- Meals App/screens/CategoriesScreen.js
+++ Meals App/screens/CategoriesScreen.js
@@ -2,27 +2,26 @@ import CategoryGridTile from '../components/CategoryGridTile'
 import { CATEGORIES } from '../data/dummy-data'
 import { FlatList } from 'react-native'
 
-
 const CategoriesScreen = ({ navigation }) => {
   function renderCategoryItem (itemData) {
-  function pressHandler () {
-    navigation.navigate('Meals Overview',{
-      categoryId: itemData.item.id,
-    })
-  }
+    function pressHandler () {
+      navigation.navigate('Meals Overview', {
+        categoryId: itemData.item.id
+      })
+    }
 
-  return (
-    <CategoryGridTile
-      title={itemData.item.title}
-      color={itemData.item.color}
-      onPress={pressHandler}
-    />
-  )
-}
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    )
+  }
   return (
     <>
       <FlatList
-      style={{backgroundColor: '#3f2f25',}}
+        style={{ backgroundColor: '#3f2f25' }}
         data={CATEGORIES}
         keyExtractor={item => item.id}
         renderItem={renderCategoryItem}
